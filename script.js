2369e59d50e2c72b9a0aad9406ab904a3937e730
Пользователь <bekzodyigitaliyev18@gmail.com>
2023-04-18 03:17:55 +0500

    tayor tugadi..

---
+++ script.js
@@ -0,0 +1,7 @@
+// ===================typing animation=======================
+var typed = new Typed(".typing",{
+    strings:[" ","wep Designer", "wep Developer", "Griphis designer", "YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
